49031e1a11dff262b1198adaa8085cf288b7374e
Ruslan Talpa <ruslan.talpa@gmail.com>
2021-02-11 12:47:18 +0200

    request.validate and optimize the other request lib functions

--- db/src/libs/request.sql
+++ db/src/libs/request.sql
@@ -6,25 +6,44 @@ create or replace function request.env_var(v text) returns text as $$
 $$ stable language sql;
 
 create or replace function request.jwt_claim(c text) returns text as $$
-    select request.env_var('request.jwt.claim.' || c);
+    select current_setting('request.jwt.claim.' || c, true);
 $$ stable language sql;
 
 create or replace function request.cookie(c text) returns text as $$
-    select request.env_var('request.cookie.' || c);
+    select current_setting('request.cookie.' || c, true);
 $$ stable language sql;
 
 create or replace function request.header(h text) returns text as $$
-    select request.env_var('request.header.' || h);
+    select current_setting('request.header.' || h, true);
 $$ stable language sql;
 
 create or replace function request.user_id() returns int as $$
     select 
-    case coalesce(request.jwt_claim('user_id'),'')
+    case coalesce(current_setting('request.jwt.claim.user_id', true),'')
     when '' then 0
-    else request.jwt_claim('user_id')::int
+    else current_setting('request.jwt.claim.user_id', true)::int
 	end
 $$ stable language sql;
 
 create or replace function request.user_role() returns text as $$
-    select request.jwt_claim('role')::text;
+    select current_setting('request.jwt.claim.role', true)::text;
 $$ stable language sql;
+
+create or replace function validate(
+  valid boolean, 
+  err text,
+  details text default '',
+  hint text default '',
+  errcode text default 'P0001'
+) returns boolean as $$
+begin
+   if valid then
+      return true;
+   else
+      RAISE EXCEPTION '%', err USING
+      DETAIL = details, 
+      HINT = hint, 
+      ERRCODE = errcode;
+   end if;
+end
+$$ stable language plpgsql;
